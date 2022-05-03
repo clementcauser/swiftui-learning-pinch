76ff66943fc3645dd4baef16c0e421fd59fec07d
Clément Causer <ccauser@simplonprod.co>
2022-05-03 19:34:46 +0200

    add drawer user interface

--- Pinch/Pinch/Screens/ContentView.swift
+++ Pinch/Pinch/Screens/ContentView.swift
@@ -12,7 +12,8 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
-    
+    @State private var isDrawerOpen: Bool = false
+
     // MARK: - FUNCTION
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -20,9 +21,9 @@ struct ContentView: View {
             imageOffset = .zero
         }
     }
-    
+
     // MARK: - CONTENT
-    
+
     var body: some View {
         NavigationView {
             ZStack {
@@ -38,62 +39,62 @@ struct ContentView: View {
                     .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
-                    .onTapGesture(count: 2, perform: {
-                        if imageScale == 1 {
-                            withAnimation(.spring()) {
-                                imageScale = 5
-                            }
-                        } else {
+                .onTapGesture(count: 2, perform: {
+                    if imageScale == 1 {
+                        withAnimation(.spring()) {
+                            imageScale = 5
+                        }
+                    } else {
+                        resetImageState()
+                    }
+                })
+                // MARK: - 2. DRAG GESTURE
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                        withAnimation(.linear(duration: 1)) {
+                            imageOffset = value.translation
+                        }
+                    }
+                        .onEnded { _ in
+                        if imageScale <= 1 {
                             resetImageState()
                         }
-                    })
-                // MARK: - 2. DRAG GESTURE
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                withAnimation(.linear(duration: 1)) {
-                                    imageOffset = value.translation
-                                }
-                            }
-                            .onEnded { _ in
-                                if imageScale <= 1 {
-                                    resetImageState()
-                                }
-                            }
-                    )
+                    }
+                )
                 // MARK: - 3. MAGNIFICATION
-                    .gesture(
-                        MagnificationGesture()
-                            .onChanged { value in
-                                withAnimation(.linear(duration: 1)) {
-                                    if imageScale >= 1 && imageScale <= 5 {
-                                        imageScale = value
-                                    } else if imageScale > 5 {
-                                        imageScale = 5
-                                    }
-                                }
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { value in
+                        withAnimation(.linear(duration: 1)) {
+                            if imageScale >= 1 && imageScale <= 5 {
+                                imageScale = value
+                            } else if imageScale > 5 {
+                                imageScale = 5
                             }
-                            .onEnded { _ in
-                                if imageScale > 5 {
-                                    imageScale = 5
-                                } else if imageScale <= 1 {
-                                    resetImageState()
-                                }
-                            }
-                    )
+                        }
+                    }
+                        .onEnded { _ in
+                        if imageScale > 5 {
+                            imageScale = 5
+                        } else if imageScale <= 1 {
+                            resetImageState()
+                        }
+                    }
+                )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
-            .navigationBarTitleDisplayMode(.inline)
-            .onAppear(perform: {
+                .navigationBarTitleDisplayMode(.inline)
+                .onAppear(perform: {
                 withAnimation(.linear(duration: 1)) {
                     isAnimating = true
                 }
             })
             // MARK: - INFO PANEL
             .overlay(InfoPanelView(scale: imageScale, offset: imageOffset)
-                .padding(.horizontal)
-                .padding(.top, 30)
-                     , alignment: .top
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
             )
             // MARK: - CONTROLS
             .overlay(
@@ -104,7 +105,7 @@ struct ContentView: View {
                             withAnimation(.spring()) {
                                 if imageScale > 1 {
                                     imageScale -= 1
-                                    
+
                                     if imageScale <= 1 {
                                         resetImageState()
                                     }
@@ -113,20 +114,20 @@ struct ContentView: View {
                         } label: {
                             ControlImageView(icon: "minus.magnifyingglass")
                         }
-                        
+
                         // RESET
                         Button {
                             resetImageState()
                         } label: {
                             ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
                         }
-                        
+
                         // SCALE UP
                         Button {
                             withAnimation(.spring()) {
                                 if imageScale < 5 {
                                     imageScale += 1
-                                    
+
                                     if imageScale > 5 {
                                         imageScale = 5
                                     }
@@ -137,12 +138,40 @@ struct ContentView: View {
                         }
                     } // MARK: CONTROLS
                     .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
-                    .background(.ultraThinMaterial)
-                    .cornerRadius(12)
-                    .opacity(isAnimating ? 1 : 0)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(12)
+                        .opacity(isAnimating ? 1 : 0)
                 }
                     .padding(.bottom, 30)
                 , alignment: .bottom
+            )
+            // MARK: - DRAWER
+            .overlay(
+                HStack(spacing: 12) {
+                    // MARK: - DRAWER HANDLE
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundColor(.secondary)
+                        .onTapGesture(perform: {
+                        withAnimation(.easeOut) {
+                            isDrawerOpen.toggle()
+                        }
+                    })
+
+                    // MARK: - THUMBNAILS
+                    Spacer()
+                }
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width: 260)
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 20 : 215)
+                , alignment: .topTrailing
             )
         } // MARK: NAVIGATION
         .navigationViewStyle(.stack)
