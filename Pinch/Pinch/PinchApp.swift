8c7678b378be54d4b9de5030b0368b8f3f03b839
Clément Causer <ccauser@simplonprod.co>
2022-05-03 16:14:36 +0200

    add double tap image zooming

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Clément Causer on 03/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
