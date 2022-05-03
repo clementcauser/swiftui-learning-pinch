c05b096f927e282fb584daa4492137bd1761126b
Clément Causer <ccauser@simplonprod.co>
2022-05-03 18:51:50 +0200

    add magnification gesture

--- Pinch/Pinch/Screens/ContentView.swift
+++ Pinch/Pinch/Screens/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
-
+    
     // MARK: - FUNCTION
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -20,9 +20,9 @@ struct ContentView: View {
             imageOffset = .zero
         }
     }
-
+    
     // MARK: - CONTENT
-
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -38,44 +38,62 @@ struct ContentView: View {
                     .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
-                .onTapGesture(count: 2, perform: {
-                    if imageScale == 1 {
-                        withAnimation(.spring()) {
-                            imageScale = 5
-                        }
-                    } else {
-                        resetImageState()
-                    }
-                })
-                // MARK: - 2. DRAG GESTURE
-                .gesture(
-                    DragGesture()
-                        .onChanged {
-                        value in
-                        withAnimation(.linear(duration: 1)) {
-                            imageOffset = value.translation
-                        }
-                    }
-                        .onEnded {
-                        _ in
-                        if imageScale <= 1 {
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
                             resetImageState()
                         }
-                    }
-                )
+                    })
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
+                // MARK: - 3. MAGNIFICATION
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
-                .navigationBarTitleDisplayMode(.inline)
-                .onAppear(perform: {
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
                 withAnimation(.linear(duration: 1)) {
                     isAnimating = true
                 }
             })
             // MARK: - INFO PANEL
             .overlay(InfoPanelView(scale: imageScale, offset: imageOffset)
-                    .padding(.horizontal)
-                    .padding(.top, 30)
-                , alignment: .top
+                .padding(.horizontal)
+                .padding(.top, 30)
+                     , alignment: .top
             )
             // MARK: - CONTROLS
             .overlay(
@@ -86,7 +104,7 @@ struct ContentView: View {
                             withAnimation(.spring()) {
                                 if imageScale > 1 {
                                     imageScale -= 1
-
+                                    
                                     if imageScale <= 1 {
                                         resetImageState()
                                     }
@@ -95,20 +113,20 @@ struct ContentView: View {
                         } label: {
                             ControlImageView(icon: "minus.magnifyingglass")
                         }
-
+                        
                         // RESET
                         Button {
                             resetImageState()
                         } label: {
                             ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
                         }
-
+                        
                         // SCALE UP
                         Button {
                             withAnimation(.spring()) {
                                 if imageScale < 5 {
                                     imageScale += 1
-
+                                    
                                     if imageScale > 5 {
                                         imageScale = 5
                                     }
@@ -119,9 +137,9 @@ struct ContentView: View {
                         }
                     } // MARK: CONTROLS
                     .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
-                        .background(.ultraThinMaterial)
-                        .cornerRadius(12)
-                        .opacity(isAnimating ? 1 : 0)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
                 }
                     .padding(.bottom, 30)
                 , alignment: .bottom
