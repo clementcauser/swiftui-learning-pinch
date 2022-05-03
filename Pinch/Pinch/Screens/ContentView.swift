81aa452c3eb9754890e2ebf98faec3fdd5c2c417
Clément Causer <ccauser@simplonprod.co>
2022-05-03 17:29:49 +0200

    add long press gesture

--- Pinch/Pinch/Screens/ContentView.swift
+++ Pinch/Pinch/Screens/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
-
+    
     // MARK: - FUNCTION
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -20,12 +20,13 @@ struct ContentView: View {
             imageOffset = .zero
         }
     }
-
+    
     // MARK: - CONTENT
-
+    
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -37,39 +38,45 @@ struct ContentView: View {
                     .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
-                .onTapGesture(count: 2, perform: {
-                    if imageScale == 1 {
-                        withAnimation(.spring()) {
-                            imageScale = 5
-                        }
-                    } else {
-                        resetImageState()
-                    }
-                })
-                // MARK: - 2. DRAG GESTURE
-                .gesture(
-                    DragGesture()
-                        .onChanged {
-                        value in
-                        withAnimation(.linear(duration: 1)) {
-                            imageOffset = value.translation
-                        }
-                    }
-                        .onEnded {
-                        _ in
-                        if imageScale <= 1 {
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
                             resetImageState()
                         }
-                    }
-                )
+                    })
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged {
+                                value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded {
+                                _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
-                .navigationBarTitleDisplayMode(.inline)
-                .onAppear(perform: {
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
                 withAnimation(.linear(duration: 1)) {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(InfoPanelView(scale: imageScale, offset: imageOffset)
+                .padding(.horizontal)
+                .padding(.top, 30)
+                     , alignment: .top
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
