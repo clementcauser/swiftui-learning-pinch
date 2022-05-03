6b5c070822d1b76f57835ba60e300890f3982444
Clément Causer <ccauser@simplonprod.co>
2022-05-03 18:08:37 +0200

    add controls interface

--- Pinch/Pinch/Screens/ContentView.swift
+++ Pinch/Pinch/Screens/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
-    
+
     // MARK: - FUNCTION
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -20,9 +20,9 @@ struct ContentView: View {
             imageOffset = .zero
         }
     }
-    
+
     // MARK: - CONTENT
-    
+
     var body: some View {
         NavigationView {
             ZStack {
@@ -38,46 +38,95 @@ struct ContentView: View {
                     .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
-                    .onTapGesture(count: 2, perform: {
-                        if imageScale == 1 {
-                            withAnimation(.spring()) {
-                                imageScale = 5
-                            }
-                        } else {
+                .onTapGesture(count: 2, perform: {
+                    if imageScale == 1 {
+                        withAnimation(.spring()) {
+                            imageScale = 5
+                        }
+                    } else {
+                        resetImageState()
+                    }
+                })
+                // MARK: - 2. DRAG GESTURE
+                .gesture(
+                    DragGesture()
+                        .onChanged {
+                        value in
+                        withAnimation(.linear(duration: 1)) {
+                            imageOffset = value.translation
+                        }
+                    }
+                        .onEnded {
+                        _ in
+                        if imageScale <= 1 {
                             resetImageState()
                         }
-                    })
-                // MARK: - 2. DRAG GESTURE
-                    .gesture(
-                        DragGesture()
-                            .onChanged {
-                                value in
-                                withAnimation(.linear(duration: 1)) {
-                                    imageOffset = value.translation
-                                }
-                            }
-                            .onEnded {
-                                _ in
-                                if imageScale <= 1 {
-                                    resetImageState()
-                                }
-                            }
-                    )
+                    }
+                )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
-            .navigationBarTitleDisplayMode(.inline)
-            .onAppear(perform: {
+                .navigationBarTitleDisplayMode(.inline)
+                .onAppear(perform: {
                 withAnimation(.linear(duration: 1)) {
                     isAnimating = true
                 }
             })
             // MARK: - INFO PANEL
             .overlay(InfoPanelView(scale: imageScale, offset: imageOffset)
-                .padding(.horizontal)
-                .padding(.top, 30)
-                     , alignment: .top
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
             )
-        } //: NAVIGATION
+            // MARK: - CONTROLS
+            .overlay(
+                Group {
+                    HStack {
+                        // SCALE DOWN
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+
+                        // RESET
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+
+                        // SCALE UP
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale < 5 {
+                                    imageScale += 1
+
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                    } // MARK: CONTROLS
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(12)
+                        .opacity(isAnimating ? 1 : 0)
+                }
+                    .padding(.bottom, 30)
+                , alignment: .bottom
+            )
+        } // MARK: NAVIGATION
         .navigationViewStyle(.stack)
     }
 }
---
+++ Pinch/Pinch/Views/ControlImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Clément Causer on 03/05/2022.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
